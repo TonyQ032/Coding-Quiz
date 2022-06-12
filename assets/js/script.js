1b9d3d310221b51c97e93464fe89c8c2969c6ef2
aq032 <anthonyq032@gmail.com>
2022-06-12 11:05:12 -0700

    Randomized questions and made them appear on site

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,201 +10,180 @@ const menuCards = [startCard, transitionCard, questionCard, resultsCard, leaderb
 const mainCard = document.querySelector("#main-card");
 const headerCard = document.querySelector("h1");
 
+// Quiz variables
+const quizQuestion = document.querySelector("#question");
+// const answerOne = document.querySelector("#answer-1");
+// const answerTwo = document.querySelector("#answer-2");
+// const answerThree = document.querySelector("#answer-3");
+// const answerFour = document.querySelector("#answer-4");
+// const answerButtons = document.querySelector(".answers");
+const correctOrNot = document.querySelector("#correct-or-not");
+let answerContainer = document.querySelector("#answer-container");
+
+
 // Non-answer buttons
 const startQuizButton = document.querySelector("#start-quiz-btn");
 const submitInitials = document.querySelector("#submit-initials");
 const playAgainButton = document.querySelector("#back-to-home");
 
+//Function variables
+let questionsUsed = [];
+let questionIndex;
+// -------------------------
+
 // All the quiz questions and answers
 let questions = [{
   question: "Which of the following is NOT a semantic HTML element?",
   answers: [{
-    text: "<div>",
-    isCorrect: true
+    text: "<div>", isCorrect: true
   }, {
-    text: "<main>",
-    isCorrect: false
+    text: "<main>", isCorrect: false
   }, {
-    text: "<header>",
-    isCorrect: false
+    text: "<header>", isCorrect: false
   }, {
-    text: "<footer>",
-    isCorrect: false
+    text: "<footer>", isCorrect: false
   }]
 }, { 
   question: "In CSS, what is the correct way to target an HTML element's ID attribute if the ID has a value of 'wrapper'?",
   answers: [{
-    text: "#wrapper",
-    isCorrect: true
+    text: ".wrapper", isCorrect: false
   }, {
-    text: ".wrapper",
-    isCorrect: false
+    text: "#wrapper", isCorrect: true
   }, {
-    text: "wrapper",
-    isCorrect: false
+    text: "!wrapper", isCorrect: false
   }, {
-    text: "!wrapper",
-    isCorrect: false
+    text: "wrapper", isCorrect: false
   }]
 }, { 
   question: "According to the CSS Box Model, what is the furthest most outside layer of a box called?",
   answers: [{
-    text: "margin",
-    isCorrect: true
+    text: "padding", isCorrect: false
   }, {
-    text: "border",
-    isCorrect: false
+    text: "border", isCorrect: false
   }, {
-    text: "padding",
-    isCorrect: false
+    text: "margin", isCorrect: true
   }, {
-    text: "content",
-    isCorrect: false
+    text: "content", isCorrect: false
   }]
 }, { 
   question: "In web development, what does the 'DOM' stand for?",
   answers: [{
-    text: "Document Object Model",
-    isCorrect: true
+    text: "Digital Orientation Map", isCorrect: false
   }, {
-    text: "Data Omission Module",
-    isCorrect: false
+    text: "Data Omission Module", isCorrect: false
   }, {
-    text: "Digital Orientation Map",
-    isCorrect: false
+    text: "Document Object Model", isCorrect: true
   }, {
-    text: "Datatype Offload Model",
-    isCorrect: false
+    text: "Datatype Offload Model", isCorrect: false
   }]
 }, { 
   question: "In JavaScript, what does the querySelector method do?",
   answers: [{
-    text: "Allows you to select and target an HTML element",
-    isCorrect: true
+    text: "Converts the value of the object into a string", isCorrect: false
   }, {
-    text: "Creates different CSS depending on the screen size",
-    isCorrect: false
+    text: "Creates different CSS depending on the screen size", isCorrect: false
   }, {
-    text: "Returns the data type of the object it is attached to",
-    isCorrect: false
+    text: "Returns the data type of the object it is attached to", isCorrect: false
   }, {
-    text: "Converts the value of the object into a string",
-    isCorrect: false
+    text: "Allows you to select and target an HTML element", isCorrect: true
   }]
 }, { 
   question: "In JavaScript, which of the following would accurately assign the Boolean value of 'true' to the variable 'answer'?",
   answers: [{
-    text: "var answer = true;",
-    isCorrect: true
+    text: "var answer = true;", isCorrect: true
   }, {
-    text: "var answer = 'true';",
-    isCorrect: false
+    text: "var answer = 'true';", isCorrect: false
   }, {
-    text: "var answer(true);",
-    isCorrect: false
+    text: "var answer(true);", isCorrect: false
   }, {
-    text: "var answer.true;",
-    isCorrect: false
+    text: "var answer.true;", isCorrect: false
   }]
 }, { 
   question: "In JavaScript, what is the index of the first item in an array?",
   answers: [{
-    text: "0",
-    isCorrect: true
+    text: "1", isCorrect: false
   }, {
-    text: "1",
-    isCorrect: false
+    text: "0", isCorrect: true
   }, {
-    text: "-1",
-    isCorrect: false
+    text: "-1", isCorrect: false
   }, {
-    text: "null",
-    isCorrect: false
+    text: "null", isCorrect: false
   }]
 }, { 
   question: "In JavaScript, which of the following is NOT a valid condition in a conditional statement?",
   answers: [{
-    text: "why",
-    isCorrect: true
+    text: "else if", isCorrect: false
   }, {
-    text: "if",
-    isCorrect: false
+    text: "if", isCorrect: false
   }, {
-    text: "else if",
-    isCorrect: false
+    text: "why", isCorrect: true
   }, {
-    text: "else",
-    isCorrect: false
+    text: "else", isCorrect: false
   }]
 }, { 
   question: "Which HTML attribute, when placed within an <a> element, would open a link in a separate tab when clicked?",
   answers: [{
-    text: "target = '_blank'",
-    isCorrect: true
+    text: "style = 'display: flex;'", isCorrect: false
   }, {
-    text: "href = './new-tab'",
-    isCorrect: false
+    text: "href = './new-tab'", isCorrect: false
   }, {
-    text: "src = 'tab+'",
-    isCorrect: false
+    text: "src = 'tab+'", isCorrect: false
   }, {
-    text: "style = 'display: flex;'",
-    isCorrect: false
+    text: "target = '_blank'", isCorrect: true
   }]
 }, { 
   question: "In CSS, what is the proper syntax for writing comments?",
   answers: [{
-    text: "/* This is the proper way to comment in CSS */",
-    isCorrect: true
+    text: "/* This is the proper way to comment in CSS */", isCorrect: true
   }, {
-    text: "// This is the proper way to comment in CSS ",
-    isCorrect: false
+    text: "// This is the proper way to comment in CSS ", isCorrect: false
   }, {
-    text: "<!-- This is the proper way to comment in CSS -->",
-    isCorrect: false
+    text: "<!-- This is the proper way to comment in CSS -->", isCorrect: false
   }, {
-    text: "# This is the proper way  to comment in CSS",
-    isCorrect: false
+    text: "# This is the proper way  to comment in CSS", isCorrect: false
   }]
 }, { 
   question: "What is the MOST specific selector in CSS?",
   answers: [{
-    text: "id",
-    isCorrect: true
+    text: "class", isCorrect: false
   }, {
-    text: "class",
-    isCorrect: false
+    text: "id", isCorrect: true
   }, {
-    text: "element",
-    isCorrect: false
+    text: "universal", isCorrect: false
   }, {
-    text: "universal",
-    isCorrect: false
+    text: "element", isCorrect: false
   }]
 }, { 
   question: "What should be the first line of an HTML document?",
   answers: [{
-    text: "<!DOCTYPE HTML>",
-    isCorrect: true
+    text: "<Hello world!>", isCorrect: false
   }, {
-    text: "<html>",
-    isCorrect: false
+    text: "<html>", isCorrect: false
   }, {
-    text: "<head>",
-    isCorrect: false
+    text: "<head>", isCorrect: false
   }, {
-    text: "<Hello world!>",
-    isCorrect: false
+    text: "<!DOCTYPE HTML>", isCorrect: true
   }]
 }];
 
+// -------------------------
+
 // Starts game
 function startGame() {
-  console.log("start");
+
+  //Allows questions to be reused if played again
+  questionsUsed = []
+
+  //Deletes previous answers generated
+  answerContainer.replaceChildren();
   startCard.removeAttribute("id", "appear");
   transitionCard.setAttribute("id", "appear");
+
   countDown();
 };
+
+
+// -------------------------
 
 // at the moment i will be making individual "nextCard" functions that change to the next card
 // however by the end of the project these should all be in one single function
@@ -237,6 +216,9 @@ function nextCard4() {
   menuCards[4].setAttribute("id", "appear");
 }
 
+
+
+
 // Returns to menu card from any card (debugging purposes)
 function backToMenuCard() {
   for (let i = 0; i < 5; i++) {
@@ -248,7 +230,6 @@ function backToMenuCard() {
 }
 
 // pre-game timer
-
 function countDown() {
   let preCountDown = 4;
 
@@ -263,6 +244,7 @@ function countDown() {
       preCountDown = 4;
       timeEl.textContent = "";
       nextCard2();
+      displayQuizCard();
     }
   }, 1000);
 }
@@ -285,8 +267,70 @@ function gameTime() {
   }, 1000);
 }
 
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Displays question on to page when used
+function displayQuestions(a) {
+  quizQuestion.textContent = questions[a].question;
+}
+
+function displayAnswers(a) {
+  let answersGenerated = questions[a].answers;
+  for (let i = 0; i < answersGenerated.length; i++) {
+    let answerButton = document.createElement('li');
+    answerButton.setAttribute("class", "answers");
+    answerButton.textContent = answersGenerated[i].text;
+    answerContainer.appendChild(answerButton);
+  }
+}
+
+// Checks to see if answer is correct
+// a = question index and b = which answer they chose
+function rightOrWrong(a, b) {
+  return questions[a].answers[b].isCorrect;
+}
+
 // determines what question will be asked
-function randomQuestion() {
+function randomNumber() {
+  return Math.floor(Math.random() * questions.length);
+}
+
+function displayQuizCard() {
+  //generates a random number from the questions array
+  questionIndex = randomNumber();
+
+  //checks to see if the question has already been used and regenerates a new number
+  if (questionsUsed.includes(questionIndex)){
+    questionIndex = randomNumber();
+  }
+
+  // Displays the current questions and answers
+  displayQuestions(questionIndex);
+  displayAnswers(questionIndex)
+
+  //Pushes the index of used question to questionsUsed to not be repeated
+  questionsUsed.push(questionIndex);
 
 }
 
@@ -297,6 +341,8 @@ startQuizButton.addEventListener("click", startGame);
 submitInitials.addEventListener("click", nextCard4);
 
 playAgainButton.addEventListener("click", backToMenuCard);
+
+
 
 function placeHolder() {
   //default card colors
